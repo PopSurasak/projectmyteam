f51d77c4e284ff4173749e9a97af6a8d6295d626
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2025-02-04 20:53:08 +0700

    Update Restaurant.java

--- projectmyteam/app/src/main/java/projectmyteam/Restaurant.java
+++ projectmyteam/app/src/main/java/projectmyteam/Restaurant.java
@@ -1,30 +1,51 @@
 package projectmyteam;
 
+import java.util.HashMap;
+
 class Restaurant {
     private String restaurantName;
     private String foodName;
-    private String foodRecipe;
     private String foodIngrediant;
-    private String totalIngrediant;
-    private float foodPrice;
+    private HashMap<String, String> foodRecipes = new HashMap<>();
+    private HashMap<String, Float> foodPrices = new HashMap<>();
 
     public void createRestaurant(String restaurantName) {
-        this.restaurantName = restaurantName;
+        if (restaurantName != null && !restaurantName.trim().isEmpty()) {
+            this.restaurantName = restaurantName;
+        } else {
+            System.out.println("Put a name or we will put it for you");
+        }
     }
 
     public void addFood(String foodName) {
-        this.foodName = foodName;
+        if (foodName != null && !foodName.trim().isEmpty()) {
+            this.foodName = foodName;
+        } else {
+            System.out.println("Nothing? Really?");
+        }
     }
 
     public void addRecipe(String foodRecipe) {
-        this.foodRecipe = foodRecipe;
+        if (foodName != null && foodRecipe != null && !foodName.trim().isEmpty() && !foodRecipe.trim().isEmpty()) {
+            foodRecipes.put(foodName, foodRecipe);
+        } else {
+            System.out.println("A proper meal?");
+        }
     }
 
     public void addIngrediant(String foodIngrediant) {
-        this.foodIngrediant = foodIngrediant;
+        if (foodIngrediant != null && !foodIngrediant.trim().isEmpty()) {
+            this.foodIngrediant = foodIngrediant;
+        } else {
+            System.out.println("What are you cooking?");
+        }
     }
 
-    public void addPrice(float foodPrice) {
-        this.foodPrice = foodPrice;
+    public void addPrice(String foodName, float foodPrice) {
+        if (foodName != null && foodPrice > 0) {
+            foodPrices.put(foodName, foodPrice);
+        } else {
+            System.out.println("Are you giving it out for free?");
+        }
     }
 }
