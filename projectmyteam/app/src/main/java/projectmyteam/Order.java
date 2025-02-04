9e463d6b87fff309a56d647e37c791c52f8231f3
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2025-02-04 20:52:18 +0700

    Update Order.java

--- projectmyteam/app/src/main/java/projectmyteam/Order.java
+++ projectmyteam/app/src/main/java/projectmyteam/Order.java
@@ -6,16 +6,23 @@ class Order {
     private int orderQuantity;
     private float orderPrice;
 
-    public void orderUser(String orderUser) {
+    public void setOrderUser(String orderUser) {
         this.orderUser = orderUser;
     }
 
-    public void orderInfo(String orderFood, int orderQuantity) {
+    public void setOrderInfo(String orderFood, int orderQuantity) {
         this.orderFood = orderFood;
         this.orderQuantity = orderQuantity;
     }
 
-    public void orderPrice(float orderPrice) {
-        this.orderPrice = orderPrice;
+    public void setOrderPrice(float unitPrice) {
+        this.orderPrice = unitPrice * orderQuantity;
+    }
+
+    public void printOrderDetails() {
+        System.out.println("Order User: " + orderUser);
+        System.out.println("Food: " + orderFood);
+        System.out.println("Quantity: " + orderQuantity);
+        System.out.println("Total Price: " + orderPrice);
     }
 }
