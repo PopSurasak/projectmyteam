12217249212ae2ec885327490fcf13bb1f1315ab
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2025-02-04 20:53:25 +0700

    Update User.java

--- projectmyteam/app/src/main/java/projectmyteam/User.java
+++ projectmyteam/app/src/main/java/projectmyteam/User.java
@@ -4,13 +4,39 @@ class User {
     private String username;
     private float userBalance;
 
+    public User(String username, float balance) {
+        if (username != null && !username.trim().isEmpty()) {
+            this.username = username;
+        } else {
+            System.out.println("Invalid username.");
+        }
+
+        if (balance >= 0) {
+            this.userBalance = balance;
+        } else {
+            System.out.println("Imagine having negative balance");
+            this.userBalance = balance;
+        }
+    }
+
     public void changeName(String username) {
-        this.username = username;
+        if (username != null && !username.trim().isEmpty()) {
+            this.username = username;
+        } else {
+            System.out.println("So your name is empty?");
+        }
     }
 
     public void addBalance(float balance) {
-        this.userBalance += balance;
+        if (balance > 0) {
+            this.userBalance += balance;
+        } else {
+            System.out.println("What are you doing?");
+        }
+    }
+
+    public void printUserInfo() {
+        System.out.println("Username: " + username);
+        System.out.println("Balance: $" + userBalance);
     }
 }
-
-
