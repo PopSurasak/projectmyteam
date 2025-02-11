2894d20af2505894f56cc4ca057522ae5215ee50
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2025-02-11 14:06:31 +0700

    Update UI.java

--- projectmyteam/app/src/main/java/projectmyteam/UI.java
+++ projectmyteam/app/src/main/java/projectmyteam/UI.java
@@ -1,9 +1,32 @@
 package projectmyteam;
 
-public class Cooking {
-    private String orderedFood;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-    public void finishCooking(String orderedFood) {
-        System.out.println("Cooking finished for: " + orderedFood);
+public class LoginUI {
+    public LoginUI(Stage primaryStage) {
+        Vbox root = new Vbox(10)
+        TextField usernameField = new TextField();
+        PasswordField passwordField = new PasswordField();
+        Button loginButton = new Button("Login");
+        Button registerButton = new Button("Register");
+        Label messageLabel = new Label();
+    }
+}
+
+public class UserUI {
+    //placeholder
+    }
+}
+
+public class RestaurantUI {
+    //placeholder
+    }
+}
+
+public class OrderUI {
+    //placeholder
     }
 }
